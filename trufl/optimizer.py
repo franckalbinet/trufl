abdc2c38a6c274d50c3de3f4ffd868fa0b6d51f7
florisabrams <41187146+florisabrams@users.noreply.github.com>
2024-06-23 19:33:33 +0200

    cleaned

--- trufl/optimizer.py
+++ trufl/optimizer.py
@@ -4,16 +4,17 @@
 __all__ = ['Optimizer']
 
 # %% ../nbs/02_optimizer.ipynb 3
+import numpy as np
+from .mcdm import score, normalize, weigh
+
+
+# %% ../nbs/02_optimizer.ipynb 4
 class Optimizer:
     def __init__(self, state):
         "Optimize the number of points for t. Provided the number of points to sample in t based on t-1, return values number of sample points."
         self.state = state
         return
     
-    def CP_distance(self):
-        "Calculate the CP distance for all te polygons in t"
-        return
-        
         
     def build_matrix(self, polygon_list):
         "Build the matrix for the optimization"
@@ -38,16 +39,65 @@ class Optimizer:
         self.benefit_vector = benefit_vector
         return
         
-    def rank(self, method = "SAW"):
-        "Rank the polygons based on the criteria"
-        import mcdm
-        # self.weight_vector = [0.5, 0.5]
         
-        ranking = mcdm.rank(self.matrix,
-                            alt_names= self.list_id,
-                            is_benefit_x = self.benefit_vector,
-                            w_vector=self.weight_vector,
-                            n_method="Linear2",
-                            s_method=method)
+    def rank(self,
+        x_matrix,
+        alt_names=None,
+        is_benefit_x=None,
+        n_method=None,
+        w_vector=None,
+        c_method=None,
+        w_method="MW",
+        s_method="SAW",
+    ):
+        """
+        Return the ranking of the alternatives, in descending order, using the
+        selected methods.
+        """
+        # Perform sanity checks
+        x_matrix = np.array(x_matrix, dtype=np.float64)
         
+        if alt_names is None:
+            alt_names = ["" + str(i + 1) for i in range(x_matrix.shape[0])]
+            
+        if len(alt_names) != x_matrix.shape[0]:
+            raise ValueError(
+                "The number of names for the alternatives does not match the "
+                + "number of rows in the decision matrix",
+            )
+
+        # If not specified, consider all criteria as benefit criteria
+        if is_benefit_x is None:
+            is_benefit_x = [True for _ in range(x_matrix.shape[1])]
+
+        # Normalize the decision matrix using the selected method
+        z_matrix, is_benefit_z = normalize(x_matrix, is_benefit_x, n_method)
+
+        # Determine the weight of each criterion
+        if w_vector is None:
+            # Weigh each criterion using the selected methods
+            w_vector = weigh(z_matrix, w_method, c_method)
+
+        # Score each alternative using the selected method
+        s_vector, desc_order = score(z_matrix, is_benefit_z, w_vector, s_method)
+
+        # Get the indices of the sorted scores
+        if desc_order:
+            r_indices = np.argsort(-s_vector)
+        else:
+            r_indices = np.argsort(s_vector)
+
+        # Create a list of tuples that includes the names of the alternatives and
+        # their corresponding scores in descending order
+        ranking = []
+        for i in range(len(alt_names)):
+            ranking.append((alt_names[r_indices[i]], s_vector[r_indices[i]]))
+
         return ranking
+
+        
+   
+
+    
+
+
